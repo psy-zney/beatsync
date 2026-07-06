242c9f4bab851b805a63b08fed5a731b0eacdb6c
Meoic <meoic@example.com>
2026-07-07 00:40:20 +0700

    feat: auto setup and release rust extractor binary

--- apps/server/yt-rust-extractor/target/release/build/serde_core-dd4a31efd5c469e6/build_script_build-dd4a31efd5c469e6.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Admin\MyProject\beatsync\apps\server\yt-rust-extractor\target\release\build\serde_core-dd4a31efd5c469e6\build_script_build-dd4a31efd5c469e6.d: C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.228\build.rs
-
-C:\Users\Admin\MyProject\beatsync\apps\server\yt-rust-extractor\target\release\build\serde_core-dd4a31efd5c469e6\build_script_build-dd4a31efd5c469e6.exe: C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.228\build.rs
-
-C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\serde_core-1.0.228\build.rs:
--- apps/server/yt-rust-extractor/target/release/yt-rust-extractor.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Admin\MyProject\beatsync\apps\server\yt-rust-extractor\target\release\yt-rust-extractor.exe: C:\Users\Admin\MyProject\beatsync\apps\server\yt-rust-extractor\src\main.rs
